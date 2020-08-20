19cefc97158494dc4a5becf6e5aa36379f5ce9f2
Ian Turner <ianturner88@gmail.com>
2020-08-20 13:40:01 -0400

    spread operator

---
+++ destructuring/destructuring.js
@@ -0,0 +1,13 @@
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F'];
+const numbers = ['1', '2', '3', '4', '5', '6'];
+
+//first 2 elements
+const [a, b] = alphabet;
+
+console.log(a);
+console.log(b);
+
+//spread operator
+const [aNum, bNumb, ...rest] = numbers;
+
+console.log(rest);
