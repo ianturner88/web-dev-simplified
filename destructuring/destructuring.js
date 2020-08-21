d548894a66352386eb806a31869197145abcd42f
Ian Turner <ianturner88@gmail.com>
2020-08-21 10:23:33 -0400

    .gitignore file created

--- destructuring/destructuring.js
+++ destructuring/destructuring.js
@@ -1,13 +1,17 @@
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F'];
 const numbers = ['1', '2', '3', '4', '5', '6'];
 
-//first 2 elements
+// first 2 elements
 const [a, b] = alphabet;
 
 console.log(a);
 console.log(b);
 
-//spread operator
+// spread operator
 const [aNum, bNumb, ...rest] = numbers;
 
 console.log(rest);
+
+// merge arrays together
+const newArray = [...alphabet, ...numbers];
+console.log(newArray);
